65d8131e821487f3d789230e701ecb9d0a56b2b8
ngparas <nickgp@gmail.com>
2015-03-04 22:14:40 -0600

    changed window_slide window sizes, added demo images

--- window_slide.m
+++ window_slide.m
@@ -13,7 +13,7 @@ defPatchSize = 28;
 %hdefPatchSize = 168;
 %vdefPatchSize = 192;
 %Set patch sizes to search with:
-patchSizeRatios = [0.1:0.25:15];
+patchSizeRatios = [0.25:0.25:10];
 %patchSizeRatios = 1;
 
 %Search step size ratio
@@ -21,6 +21,7 @@ patchSizeRatios = [0.1:0.25:15];
 
 %Find size of image
 imgSize = size(imgMat);
+
 
 %loop for multiple patch sizes
 for ratioInd = 1:length(patchSizeRatios)
@@ -37,9 +38,9 @@ for ratioInd = 1:length(patchSizeRatios)
    hLoc = 1;   
    
    %slide patch down the image
-   while (vLoc + patchSize - 1) <= (imgSize(1))
+   while ((vLoc + patchSize - 1) <= (imgSize(1)))
        %slide patch across the image
-       while (hLoc + patchSize - 1) <= (imgSize(2))
+       while ((hLoc + patchSize - 1) <= (imgSize(2)))
            %get normalized vector of patch
            patch = vec_norm(imresize(imgMat(vLoc:(vLoc + patchSize - 1),hLoc:(hLoc + patchSize -1)),[28 28]));
            %determine if the patch has a face or not
@@ -47,6 +48,7 @@ for ratioInd = 1:length(patchSizeRatios)
            %if theres a face, update faceCoord
            if isFace == 1
                faceCoord(vLoc:(vLoc + patchSize - 1),hLoc:(hLoc + patchSize -1)) = 1;
+              
            end
            %increment horizontal location
            hLoc = hLoc + stepSize;
@@ -57,12 +59,12 @@ for ratioInd = 1:length(patchSizeRatios)
 end
 
 %if nothing found, try whole image
-patch = [1; vec_norm(imresize(imgMat,[28 28]))];
+%patch = [1; vec_norm(imresize(imgMat,[28 28]))];
 
-isFace = classifySVM(patch,model);
-if isFace == 1
-    faceCoord = ones(size(imgMat));
-end
+%isFace = classifySVM(patch,model);
+%if isFace == 1
+%    faceCoord = ones(size(imgMat));
+%end
 
 
 
